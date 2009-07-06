63c97cc675e19f28e5c69d84fbb443f285f4de6a
Doug Ramsay <doug@intridea.com>
2009-07-06 16:51:36 -0400

    Using S3 for Post image storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
-  has_attached_file :image, :styles => {:large => "500x500", :medium => "250x250", :thumb => "100x100"}
+  has_attached_file :image, 
+                    :styles => {:large => "500x500", :medium => "250x250", :thumb => "100x100"}, 
+                    :storage => 's3', 
+                    :s3_credentials => YAML.load_file("#{RAILS_ROOT}/config/amazon_s3.yml")[RAILS_ENV], 
+                    :bucket => "built-for-speed", 
+                    :path => ":class/:id/:style.:extension"
 end
