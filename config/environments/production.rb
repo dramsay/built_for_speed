6a9432f4bf7e23e2d0e6c6d2ac3d7b05bac354ec
Doug Ramsay <doug@intridea.com>
2009-07-08 16:12:09 -0400

    Updating production environment to use CloudFront as asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,4 +25,6 @@ config.action_view.cache_template_loading            = true
 # config.action_mailer.raise_delivery_errors = false
 
 # Enable threaded mode
-# config.threadsafe!
+# config.threadsafe! 
+
+ActionController::Base.asset_host = Proc.new { CLOUDFRONT_DISTRIBUTION }
